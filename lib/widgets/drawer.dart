780a64d83197717b11d60d6c9e3326c9c6295fbd
unknown <Administrator@LAB2-PC5.labs.org>
2025-09-10 20:33:29 +0500

    chore: wip local changes before rebase
    # Conflicts:
    #	lib/pages/contact_page.dart
    #	lib/widgets/drawer.dart

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,7 +1,6 @@
 import 'package:auth_reset_pass/pages/career_bank.dart';
 import 'package:flutter/material.dart';
 import '../pages/testimonials.dart';
-import '../pages/contact_page.dart';
 
 class UserDrawer extends StatelessWidget {
   final Function(String) onMenuItemSelected;
@@ -28,7 +27,7 @@ class UserDrawer extends StatelessWidget {
             color: Colors.black.withOpacity(0.4),
             blurRadius: 10,
             offset: const Offset(3, 0),
-          ),
+          )
         ],
       ),
       child: SafeArea(
@@ -69,12 +68,7 @@ class UserDrawer extends StatelessWidget {
             _sectionHeader("Admission & Coaching"),
             _subMenuItem(context, Icons.school, "Stream Selector"),
             _subMenuItem(context, Icons.article, "CV Tips"),
-            _subMenuItem(
-              context,
-              Icons.record_voice_over,
-              "Interview Preparation",
-            ),
-            _subMenuItem(context, Icons.book, "Content"),
+            _subMenuItem(context, Icons.record_voice_over, "Interview Preparation"),
 
             // 3) Resources Hub
             _sectionHeader("Resources Hub"),
@@ -103,12 +97,7 @@ class UserDrawer extends StatelessWidget {
 
             // 8) Contact
             _sectionHeader("Contact"),
-            _menuItem(
-              context,
-              Icons.contact_mail,
-              "Contact",
-              page: const ContactUsPage(),
-            ),
+            _menuItem(context, Icons.contact_mail, "Contact"),
           ],
         ),
       ),
@@ -131,12 +120,7 @@ class UserDrawer extends StatelessWidget {
   }
 
   // Main menu item (with optional navigation page)
-  Widget _menuItem(
-    BuildContext context,
-    IconData icon,
-    String title, {
-    Widget? page,
-  }) {
+  Widget _menuItem(BuildContext context, IconData icon, String title, {Widget? page}) {
     return ListTile(
       leading: Icon(icon, color: Colors.lightBlueAccent),
       title: Text(
