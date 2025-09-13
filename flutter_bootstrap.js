a5ebea4066a72278b070913f48dfaf68b5e3ce37
Taha1707 <Taha1707@users.noreply.github.com>
2025-09-13 06:55:25 +0000

    deploy: 07cd5c87faa9b838c7ee93b8de0e48cbb58d2747

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3905886763"
+    serviceWorkerVersion: "1948674709"
   }
 });
